a8845c4f716f4eeff0791009e6bfc66e300fbdf9
zabotini <rafaelzabotini@gmail.com>
2024-04-07 00:50:08 -0300

    Versão funcional do gerenciaBanco, sem tratamento na entrada de dados

--- java/gerenciabanco/src/main/java/com/gerenciabanco/App.java
+++ java/gerenciabanco/src/main/java/com/gerenciabanco/App.java
@@ -18,24 +18,22 @@ public class App
         */
 
         public int id;
+        public int agencia = 0001;
         public String conta;
-        private String cpf;
         private String nome;
         private String ultimoNome;
-        private double saldo;
+        private double saldo = 0.0;
 
 
         public Cliente () {
-            /**this.id = id;
-            this.cpf = cpf;
-            this.nome = nome;
-            this.ultimoNome = ultimoNome;
-            this.conta = conta;
-            this.saldo = 0.0;*/
         }
 
-        public void setCpf(String cpf) {
-            this.cpf = cpf;
+        public void setID (int id) {
+            this.id = id;
+        }
+
+        public void setConta (String conta) {
+            this.conta = conta;
         }
         
         public void setnome(String nome) {
@@ -51,11 +49,12 @@ public class App
         }
 
         public double getSaldo() { return saldo; }
+
         /** Fim da Classe do Cliente */
 
     }
 
-    public static class OperacoesBancarias {
+    static class OperacoesBancarias {
         /**
          * Aqui declararemos as operações que o cliente pode executar e também uma saída segura de sua sessão
          */
@@ -66,69 +65,120 @@ public class App
             System.out.println("Seu saldo atual é: R$" + cliente.getSaldo());
         }
 
-        static double deposito(Cliente cliente) {
+        static void deposito(Cliente cliente) {
 
             System.out.println("Digite o valor a ser depositado: ");
             double valor = scanner.nextDouble();
             if (valor > 0) {
+                cliente.setSaldo(+valor);
                 System.out.println("Foi depositada a quantia de R$" + valor + " em sua conta.\nObrigado por utilizar nossos serviços! \n");
-                cliente.setSaldo(valor);
-                return valor;
             } else {
-                throw new IllegalArgumentException("Valor inválido para depósito, tente novamente");
+                System.out.println("Valor inválido para depósito, tente novamente\n\n\n");
             }
         }
 
-        static double saque(Cliente cliente) {
+        static void saque(Cliente cliente) {
 
             System.out.println("Digite o valor a ser sacado: ");
             double valor = scanner.nextDouble();
             System.out.println("Valor do saque R$" + valor);
             if (valor > 0 && valor < cliente.saldo) {
-                System.out.println("Foi sacada a quantia de R$" + valor + " em sua conta.\nSeu saldo atualizado é de R$" + cliente.getSaldo() + "\nObrigado por utilizar nossos serviços! \n");
                 cliente.setSaldo(-valor);
-                return valor;
+                System.out.println("Foi sacada a quantia de R$" + valor + " em sua conta.\nSeu saldo atualizado é de R$" + cliente.getSaldo() + "\nObrigado por utilizar nossos serviços! \n");
             } else {
-                throw new IllegalArgumentException("Valor maior que seu saldo ou inválido para saque, tente novamente");
+                System.out.println("Valor maior que seu saldo ou inválido para saque, tente novamente");
             }
         }
 
         static void encerra() {
-            System.out.println("Obrigado por utilizar nossos serviços! Até uma próxima :)");
+            System.out.println("Obrigado por utilizar nossos serviços! Até uma próxima :)\n\n\n");
             scanner.close();
         }
+    }
+
+    static class Menu {
+        public static Scanner entradaSelecao = new Scanner(System.in);
+        public static int entradaUsuario = 0;
+
+        static void apresentaMenu(Cliente cliente){
+            System.out.printf("\nBem-vindo %s %s ao Seu Banco\n", cliente.nome, cliente.ultimoNome);
+            System.out.printf("Agência %d | Conta: %s | Saldo: %9.2f%n\n\n", cliente.agencia, cliente.conta, cliente.getSaldo());
+            System.out.println("1\tConsultar saldo\n2\tEfetuar depósito\n3\tEfetuar saque\n4\tFinalizar sessão\n");
+        }
+
+        static void selecaoMenu(Cliente cliente) {
+
+            do {
+                System.out.printf("Digite o número da funcionalidade desejada: ");
+                entradaUsuario = entradaSelecao.nextInt();
+                if (entradaUsuario > 0 && entradaUsuario < 5) {
+
+                    switch (entradaUsuario) {
+                        case 1:
+                            OperacoesBancarias.saldo(cliente);
+                            clearConsole();
+                            apresentaMenu(cliente);
+                            break;
+                        case 2:
+                            OperacoesBancarias.deposito(cliente);
+                            clearConsole();
+                            apresentaMenu(cliente);
+                            break;
+                        case 3:
+                            OperacoesBancarias.saque(cliente);
+                            clearConsole();
+                            apresentaMenu(cliente);
+                            break;
+                        case 4:
+                            OperacoesBancarias.encerra();
+                            break;
+                        default:
+                            System.out.println("Algo inesperado aconteceu, tente delisgar e ligar novamente este caixa :)\n");
+                            entradaSelecao.close();
+                            break;
+                    }
+            
+                } else {
+                    System.out.println("Número inválido, tente novamente\n");
+                    clearConsole();
+                    apresentaMenu(cliente);
+                    selecaoMenu(cliente);
+                }
+            } while (entradaUsuario > 0 && entradaUsuario < 4);
+            entradaSelecao.close();
+        }
+
+        public final static void clearConsole(){
+            try
+            {
+                final String os = System.getProperty("os.name");
+                
+                if (os.contains("Windows"))
+                {
+                    Runtime.getRuntime().exec("cls");
+                }
+                else
+                {
+                    Runtime.getRuntime().exec("clear");
+                }
+            }
+            catch (final Exception e) { }
+        }
+
+        static void clienteTeste(Cliente cliente){
+            cliente.setID(900000001);
+            cliente.setConta("12329-6");
+            cliente.setnome("Rafael");
+            cliente.setUltimoNome("Venjenski");
+        }
+
     }
 
     public static void main( String[] args )
     { 
         Cliente um = new Cliente();
-        um.setCpf("123.456.789-01");
-        um.setnome("Rafa");
-        um.setUltimoNome("Z");
-        System.out.println("ID do Cliente: " + um.id);
-        System.out.println(um.conta);
-        System.out.println(um.saldo %.2f);
-        System.out.println(um.cpf);
-        System.out.println(um.nome + " " + um.ultimoNome + "\n");
-
-        Cliente dois = new Cliente();
-        dois.setCpf("123.456.789-01");
-        dois.setnome("Abublebe");
-        dois.setUltimoNome("Doido");
-        System.out.println("ID do Cliente: " + dois.id);
-
-        System.out.println(dois.conta);
-        System.out.println(um.saldo);
-        System.out.println(dois.cpf);
-        System.out.println(dois.nome + " " + dois.ultimoNome + "\n");
-
-        OperacoesBancarias.deposito(um);
-
-        OperacoesBancarias.saldo(um);
-
-        OperacoesBancarias.saque(um);
-
-        OperacoesBancarias.saldo(um);
-
+        Menu.clienteTeste(um);
+        Menu.apresentaMenu(um);
+        Menu.selecaoMenu(um);
     }
 }
